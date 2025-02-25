2f5a1646c772338770825920337cf22a0e7b2cd1
OLeg Voroshilov <fenix6076@yandex.ru>
2025-02-25 12:15:34 +0400

    Добавлен в фикстуры управление разрешение окна браузера

--- conftest.py
+++ conftest.py
@@ -1,16 +1,18 @@
 import pytest
 from playwright.sync_api import sync_playwright
-import allure
+
 
 @pytest.fixture(scope="session")
 def browser():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False)
         yield browser
         browser.close()
 
 @pytest.fixture
 def page(browser):
     page = browser.new_page()
+    page.set_viewport_size({"width": 1920, "height": 1080})
+
     yield page
     page.close()
